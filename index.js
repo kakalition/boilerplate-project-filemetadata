82016b1c31ee9a59cd222fd36e0d6e3c3429466c
Kaka <kakalition@gmail.com>
2024-07-22 20:05:18 +0700

    patch

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 
 var app = express();
 
+const multer = require("multer");
+const upload = multer({ dest: "public/uploads/" });
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,6 +14,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const file = req.file
+
+  const output = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  }
+
+  console.log(output)
+
+  res.json(
+    output
+  );
+});
 
 
 
